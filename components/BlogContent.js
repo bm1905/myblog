abab82bd4dc8c26fb3e70f1e90f5e3ebdb778bef
Bijay Maharjan <bijay.maharjan5@gmail.com>
2020-08-20 18:32:31 -0500

    Fix Code Syntax

--- components/BlogContent.js
+++ components/BlogContent.js
@@ -6,30 +6,29 @@ const serializers = {
     types: {
         code: ({ node: { language, code, filename } }) => {
             return (
-                <HighlightCode language={language}>
-                    {code}
+                <>
+                    <HighlightCode language={language}>
+                        {code}
+                    </HighlightCode>
                     <div className="code-filename">{filename}</div>
-                </HighlightCode>
+                </>
             )
         },
         image: ({ node: { asset, alt, position = 'center' } }) => {
             return (
                 <div className={`blog-image blog-image-${position}`}>
-                    <img className="blog-image-width" src={urlFor(asset).height(800).fit('max').url()} />
+                    <img src={urlFor(asset).height(800).fit('max').url()} />
                     <div className="image-alt">{alt}</div>
                 </div>
             )
         }
     }
-};
+}
 
-const BlogContent = ({ content }) => {
-    return (
-        <BlockContent
-            serializers={serializers}
-            blocks={content}
-        />
-    )
-};
+const BlogContent = ({ content }) =>
+    <BlockContent
+        serializers={serializers}
+        blocks={content}
+    />
 
 export default BlogContent;
--- components/HighlightCode.js
+++ components/HighlightCode.js
@@ -2,22 +2,23 @@ import highlight from 'highlight.js';
 import { createRef, useEffect } from 'react';
 import { findDOMNode } from 'react-dom';
 
-const HighlightCode = ({ children, langauge }) => {
+const HighlightCode = ({ children, language }) => {
     const code = createRef();
 
     useEffect(() => {
         highlight.highlightBlock(findDOMNode(code.current));
-    }, []);
+    }, [])
 
     return (
         <pre>
             <code
                 ref={code}
-                className={langauge}>
+                className={language}>
                 {children}
             </code>
         </pre>
     )
-};
+}
+
 
 export default HighlightCode;
